25de78ccdc1f4d14a5c4adf5d93fe73f98f364b6
MaksymKurny <4680387@stud.nau.edu.ua>
2025-02-15 02:48:04 +0200

    add more player func

--- scripts/prefabs/winslow.lua
+++ scripts/prefabs/winslow.lua
@@ -13,7 +13,6 @@ local prefabs =
 	"winslow_orchestraproxy_violin",
 }
 
-
 local start_inv = {}
 for k, v in pairs(TUNING.GAMEMODE_STARTING_ITEMS) do
 	start_inv[string.lower(k)] = v.WINSLOW
@@ -97,9 +96,60 @@ local function RemoveWinslowPets(inst)
 	end
 end
 
+local function OnAttacked(inst)
+	if inst.components.petleash and math.random() < TUNING.INST_SHATTER_CHANCE then
+		local pets = inst.components.petleash:GetPets()
+		local pet_list = {}
+
+		for pet, _ in pairs(pets) do
+			table.insert(pet_list, pet)
+		end
+
+		if #pet_list > 0 then
+			local random_pet = pet_list[math.random(#pet_list)]
+			if random_pet and random_pet.RemoveWinslowPet then
+				random_pet:RemoveWinslowPet()
+			end
+		end
+		if inst.components.sanity then
+			inst.components.sanity:DoDelta(-TUNNING.SANITY_TINY)
+		end
+	end
+end
+
+local function OnAllyAttacked(inst)
+	if inst and inst.components.health and not inst.components.health:IsDead() and inst._sanity_watcher and inst._sanity_watcher.components.sanity then
+		inst._sanity_watcher.components.sanity:DoDelta(-TUNNING.SANITY_SMALL)
+	end
+end
+
+local POWERPOINT_MUST_TAGS = { "player" }
+local POWERPOINT_CAN_TAGS = { "INLIMBO", "FX", "playerghost" }
+
+local function UpdateWatchList(inst)
+	if not TheWorld.ismastersim then return end
+
+	local x, y, z = inst.Transform:GetWorldPosition()
+	local ents = TheSim:FindEntities(x, y, z, TUNING.ALLY_INFLUENCE_RADIUS, POWERPOINT_MUST_TAGS, POWERPOINT_CAN_TAGS)
+
+	for entity, _ in pairs(inst._watching) do
+		if not table.contains(ents, entity) then
+			entity:RemoveEventCallback("attacked", OnAllyAttacked)
+			inst._watching[entity] = nil
+		end
+	end
+
+	for _, entity in ipairs(ents) do
+		if not inst._watching[entity] then
+			inst._watching[entity] = true
+			entity._sanity_watcher = inst
+			entity:ListenForEvent("attacked", OnAllyAttacked)
+		end
+	end
+end
+
 local common_postinit = function(inst)
 	inst:AddTag("conductor")
-	inst:AddTag("roteater")
 	inst.MiniMapEntity:SetIcon("winslow.tex")
 end
 
@@ -112,13 +162,7 @@ local master_postinit = function(inst)
 	inst.components.hunger:SetMax(TUNING.WINSLOW_HUNGER)
 	inst.components.sanity:SetMax(TUNING.WINSLOW_SANITY)
 
-	-- inst.components.foodaffinity:AddFoodtypeAffinity(FOODTYPE.GENERIC, 1)
-	-- inst.components.foodaffinity:AddFoodtypeAffinity(FOODTYPE.MEAT, 1)
-	-- inst.components.foodaffinity:AddFoodtypeAffinity(FOODTYPE.VEGGIE, 1)
-	-- inst.components.foodaffinity:AddFoodtypeAffinity(FOODTYPE.SEEDS, 1)
-	-- inst.components.foodaffinity:AddFoodtypeAffinity(FOODTYPE.BERRY, 1)
-	-- inst.components.foodaffinity:AddFoodtypeAffinity(FOODTYPE.RAW, 1)
-	-- inst.components.foodaffinity:AddFoodtypeAffinity(FOODTYPE.GOODIES, 1)
+	inst.components.foodaffinity:AddPrefabAffinity("spoiled_food", -TUNING.AFFINITY_15_CALORIES_VERYHUGE)
 
 	if inst.components.eater ~= nil then
 		inst.components.eater.custom_stats_mod_fn = OnEatFood
@@ -135,10 +179,13 @@ local master_postinit = function(inst)
 	petleash:SetOnDespawnFn(OnDespawnPet)
 	petleash:SetOnRemovedFn(OnRemovedPet)
 	petleash:SetMaxPetsForTag("orchestra", TUNING.ORCHESTRA_LIMIT)
-	-- petleash:SetMaxPets(TUNING.ORCHESTRA_LIMIT)
 
 	inst:ListenForEvent("ms_playerreroll", RemoveWinslowPets)
 	inst:ListenForEvent("death", RemoveWinslowPets)
+
+	inst._watching = {}
+	inst:DoPeriodicTask(1, UpdateWatchList)
+	inst:ListenForEvent("attacked", OnAttacked)
 
 	inst.RecalculateOrchestraPattern = RecalculateOrchestraPattern
 
